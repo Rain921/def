0e75b174cd1406ae57a257e046cbc729de539c0a
Rain921 <56>
2018-08-04 14:59:33 +0300

    def

--- defaut/footer.php
+++ defaut/footer.php
@@ -2,6 +2,5 @@
 	
 </footer>
 <script src="/dist/js/app.min.js"></script>
-<script src="dist/js/uikit.min.js"></script>
 </body>
 </html>
--- defaut/gulpfile.js
+++ defaut/gulpfile.js
@@ -18,13 +18,14 @@ const Config = {
     build: 'dist'
   },
   reload: {
-    proxy: 'enigma.sl',
+    proxy: 'proect.sl',
     port: 3000
   },
   scripts: [
     // plugins
     'node_modules/jquery/dist/jquery.js',
     'node_modules/aos/dist/aos.js',
+    'node_modules/slick-carousel/slick/slick.min.js',
     'node_modules/jquery-validation/dist/jquery.validate.min.js',
     'node_modules/jquery.maskedinput/src/jquery.maskedinput.js',
     // scripts
